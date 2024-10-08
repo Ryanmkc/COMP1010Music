58844969d15a2777d783813f8ea14d135eeb3c7d
AndreyMQU <andrey.yermolenko@students.mq.edu.au>
2024-10-08 17:40:23 +1100

    ll
    ll

--- objects/User.java
+++ objects/User.java
@@ -1,6 +1,7 @@
 package objects;
 import java.util.ArrayList;
 
+
 public class User {
 
     String firstname;
@@ -10,18 +11,22 @@ public class User {
     String bio;
 
     ArrayList<Artist> followingArtist = new ArrayList<Artist>();
+    ArrayList<Album> likedAlbum = new ArrayList<Album>();
+
     ArrayList<User> followingUser = new ArrayList<User>();
     ArrayList<User> followedByUser = new ArrayList<User>();
 
-    ArrayList<Playlist> created = new ArrayList<Playlist>();
+    ArrayList<Playlist> created = new ArrayList<Playlist>(); // every usee will have a 2 liked playlist 
+    // ugh I need to make a liked playlist for podcasts
     ArrayList<Playlist> collabing = new ArrayList<Playlist>();
     ArrayList<Playlist> followingPlaylist = new ArrayList<Playlist>();
 
-    ArrayList<Album> likedAlbum = new ArrayList<Album>();
-    ArrayList<song> likedSong = new ArrayList<song>();
+    ArrayList<studio> followingStudio = new ArrayList<studio>();
+    ArrayList<Podcast> followedingPodcast = new ArrayList<Podcast>();
+    //ArrayList<PodcastEp> followedByUser = new ArrayList<PodcastEp>(); this will be in liked playlist
 
     Date dob;
-    boolean isMinor;
+    Boolean isMinor;
     String email;
     
 }
