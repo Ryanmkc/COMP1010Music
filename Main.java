9fa18222f2275568a3b24a4858eadde9505abcdc
AndreyMQU <andrey.yermolenko@students.mq.edu.au>
2024-10-09 20:43:44 +1100

    made 2 run files one for objects one for the general stuff

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 import objects.*;
-import javax.swing.*;
-import java.awt.image.BufferedImage;
+
 
 public class Main {
     public static void main(String[] args) {
-        
-    }
+
+        String[] objectcollection = {}; // You can pass arguments if needed
+        ObjectCollection.main(objectcollection);  // Calling ClassTwo's main method
+
+}
 }
---
+++ ObjectCollection.java
@@ -0,0 +1,9 @@
+import objects.*;
+
+public class ObjectCollection {
+public static void main(String[] args) {
+
+// we will create all the objects here to not make main more cluttered then nessesery
+
+}
+}
